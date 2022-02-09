f61a9f8dd4098f8faa8fbd33ede07bc7c077ef8f
Brinkley Hill <hillgangbro@gmail.com>
2022-02-09 08:19:10 -0500

    Complete Revamp
    Changed the layout and stuff

--- layout.js
+++ layout.js
@@ -1,25 +1,98 @@
-//output section
-const balance = document.querySelector(".balance .value");
-const incomeTotal = document.querySelector(".income-total");
-const outcomeTotal = document.querySelector(".outcome-total");
-const chart = document.querySelector(".chart");
-//dashboard buttons
-const expensesBtn = document.querySelector(".etab");
-const incomeBtn = document.querySelector(".itab");
-const allBtn = document.querySelector(".atab");
-//dashboard lists
-const expenseList = document.querySelector("#expenses .list");
-const incomeList = document.querySelector("#income .list");
-const allList = document.querySelector("#all .list");
-//dashboard inputs
-const expense = document.querySelector("#expenses");
-const income = document.querySelector("#income");
-const all = document.querySelector("#all");
-//add income
-const addIncome = document.querySelector(".add-income");
-const incomeTitle = document.getElementById("income-title-input");
-const incomeAmount = document.getElementById("income-amount-input");
-//add expense
-const addExpense = document.querySelector(".add-expense");
-const expenseTitle = document.getElementById("expense-title-input");
-const expenseAmount = document.getElementById("expense-amount-input");
+const balance = document.getElementById("balance");
+const money_plus = document.getElementById("money-plus");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+const money_minus = document.getElementById("money-minus");
+
+// const randomTransations=[
+//     {id:1,text:'salary',amount:400},
+//     {id:2,text:'books',amount:-50},
+//     {id:3,text:'paper',amount:-20},
+//     {id:4,text:'food',amount:-100},
+// ]
+const localStorageTransations = JSON.parse(localStorage.getItem("transations"));
+
+let transations =
+  localStorage.getItem("transations") !== null ? localStorageTransations : [];
+
+//add transation
+function addTransation(e) {
+  e.preventDefault();
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    text.placeholder = "please add a text";
+    text.style.backgroundColor = "#ccc";
+    amount.placeholder = "please add amount";
+    amount.style.backgroundColor = "#ccc";
+  } else {
+    const transation = {
+      id: genenrateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+    transations.push(transation);
+    addTransationDOM(transation);
+    updateValues();
+    updateLocalStorage();
+    text.value = "";
+    amount.value = "";
+  }
+}
+//generate id
+function genenrateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+//add transations to dom list
+function addTransationDOM(transation) {
+  //get sign
+  const sign = transation.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+  //add class based on value
+  item.classList.add(transation.amount < 0 ? "minus" : "plus");
+  item.innerHTML = `${transation.text} <span>${sign}${Math.abs(
+    transation.amount
+  )}</span> <button class="delete-btn" onclick="removeTransation(${
+    transation.id
+  })">x</button>`;
+  list.appendChild(item);
+}
+//update the balance
+function updateValues() {
+  const amounts = transations.map((transation) => transation.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+//remove
+function removeTransation(id) {
+  transations = transations.filter((transation) => transation.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+//updatelocal storage
+function updateLocalStorage() {
+  localStorage.setItem("transations", JSON.stringify(transations));
+}
+
+//init
+function init() {
+  list.innerHTML = "";
+  transations.forEach(addTransationDOM);
+  updateValues();
+}
+init();
+
+form.addEventListener("submit", addTransation);
