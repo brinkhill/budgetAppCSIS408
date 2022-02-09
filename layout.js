317299b6072831df8a23afd1d29a54ce553e4541
amhofflinger <91756415+amhofflinger@users.noreply.github.com>
2022-02-09 08:40:42 -0500

    Update layout.js

--- layout.js
+++ layout.js
@@ -3,22 +3,27 @@ const balance = document.querySelector(".balance .value");
 const incomeTotal = document.querySelector(".income-total");
 const outcomeTotal = document.querySelector(".outcome-total");
 const chart = document.querySelector(".chart");
+
 //dashboard buttons
 const expensesBtn = document.querySelector(".etab");
 const incomeBtn = document.querySelector(".itab");
 const allBtn = document.querySelector(".atab");
+
 //dashboard lists
 const expenseList = document.querySelector("#expenses .list");
 const incomeList = document.querySelector("#income .list");
 const allList = document.querySelector("#all .list");
+
 //dashboard inputs
 const expense = document.querySelector("#expenses");
 const income = document.querySelector("#income");
 const all = document.querySelector("#all");
+
 //add income
 const addIncome = document.querySelector(".add-income");
 const incomeTitle = document.getElementById("income-title-input");
 const incomeAmount = document.getElementById("income-amount-input");
+
 //add expense
 const addExpense = document.querySelector(".add-expense");
 const expenseTitle = document.getElementById("expense-title-input");
